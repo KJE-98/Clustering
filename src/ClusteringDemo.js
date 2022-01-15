3f4cbf1359449544610b417cfd9f1ecfe24f72ad
KJE-98 <>
2022-01-15 01:40:54 -0500

    kmeans clustering

--- src/ClusteringDemo.js
+++ src/ClusteringDemo.js
@@ -14,21 +14,191 @@ import {
   Box,
   Zoom,
   Slide,
-  Container
+  Container,
+  ButtonGroup,
+  Button
 } from '@mui/material';
 import Grid from './Grid.js';
 
 export default class ClusteringDemo extends React.Component {
+  gridState = new Array(10000).fill(0);
+  gridAnimations = [];
+  colorMappings = ['white', "black", "blue", "red", "green", "purple", "yellow"];
+
+  copy(grid){
+    let result = [];
+    for (let element of grid){
+      result.push(element);
+    }
+    return result;
+  }
+
+  indexToxy(index){
+    let y = Math.floor(index/50) + .5;
+    let x = index%50 + .5;
+    return([x,y]);
+  }
+
+  interpretAnimation = (function(queue){
+    if (queue.length == 0){return}
+    let next = queue.shift();
+    console.log(next);
+    if (next.type == "point"){
+      this.changeGrid(next.position, next.color)
+      this.interpretAnimation(queue);
+    }else if (next.type == "break"){
+      setTimeout(this.interpretAnimation, next.duration, queue);
+    }else if (next.type == "x"){
+      console.log("hello");
+      this.addAnimation("x", next.position, next.color);
+      this.interpretAnimation(queue);
+    }
+  }).bind(this);
+
+  k_means(k){
+    let animationQueue = []
+    let grid = this.copy(this.gridState);
+    //create n random points to be the initial centers
+    let centerArray = new Array(k).fill(0).map(() => [Math.random()*50,Math.random()*50])
+    let goAgain = true;
+    // loop
+    while (goAgain == true){
+      goAgain = false;
+      let centerColor = 2;
+      for (let point of centerArray){
+        console.log(point);
+        animationQueue.push({
+          type: "x",
+          position: point,
+          color:  centerColor
+        });
+        centerColor++;
+      }
+      animationQueue.push({
+        type: "break",
+        duration: 1000,
+      });
+      let newCenterArray = new Array(k).fill(0).map(() => [0,0]);
+      let newCenterArrayHelper = new Array(k).fill(0);
+      let newGrid = this.copy(grid);
+      for(const [index,point] of grid.entries()){
+        if (point == 0) {continue;}
+
+        let coordinates = (this.indexToxy(index));
+        let distance = 100000;
+        let closest = 2;
+        let clusterIndex = 2;
+
+        for (let center of centerArray){
+          let newDistance = (center[0]-coordinates[0])**2 + (center[1]-coordinates[1])**2;
+          if (newDistance<distance){
+            closest = clusterIndex;
+            distance = newDistance;
+          }
+          clusterIndex++;
+        }
+        newGrid[index] = closest;
+        if (closest != point){
+          goAgain = true;
+          animationQueue.push({
+            type: "point",
+            color: closest,
+            position: index,
+          });
+        }
+        newCenterArray[closest-2][0] += coordinates[0];
+        newCenterArray[closest-2][1] += coordinates[1];
+        newCenterArrayHelper[closest-2] += 1;
+      }
+      centerArray = newCenterArray.map((element, index) => {
+        if (newCenterArrayHelper[index]==0){return [Math.random()*50,Math.random()*50]}
+        return [element[0]/newCenterArrayHelper[index],element[1]/newCenterArrayHelper[index]]
+      });
+      grid = newGrid;
+      animationQueue.push({
+        type: "break",
+        duration: 1000,
+      });
+    }
+    this.interpretAnimation(animationQueue);
+    return animationQueue;
+  }
+
+  mean_shift(r){
+    
+  }
+
+  hoistSpace = (space)=> {
+    this.gridSpace = space;
+  }
 
   changeAlg = (function(event) {
-    this.setState({algType: event.target.value})
-    console.log("hi")
+    this.setState({algType: event.target.value});
+    this.addAnimation("dot", 150);
+    this.createRandomPoints(300);
   }).bind(this);
+
+  changeGrid = (function(index, key) {
+    this.addAnimation("dot", index);
+    this.gridState[index] = key;
+  }).bind(this);
+
+  clearGrid = (function(indexStart) {
+    if (indexStart>9999) {
+      return
+    }
+    for (let i = indexStart; i<indexStart+100; i++){
+      if (this.gridState[i] != 0){
+        this.addAnimation("dot", i);
+        this.gridState[i] = 0;
+      }
+    }
+    setTimeout(this.clearGrid,10,indexStart+100);
+  }).bind(this);
+
+  addAnimation = function(type, position, color){
+    let animationObj = {}
+    if (type == "dot"){
+      animationObj = {
+        progress: 0,
+        duration: 350,
+        type: "dot",
+        index: position,
+        color: this.colorMappings[this.gridState[position]]
+      }
+    }else if (type == "x"){
+      console.log("addAnimation");
+      animationObj = {
+        progress: 0,
+        duration: 1000,
+        type: 'x',
+        position: position,
+        color: color,
+      }
+    }
+    this.gridAnimations.push(animationObj)
+  }
+
+  deleteAnimation = (function(index){
+    this.gridAnimations.splice(index,1);
+  }).bind(this);
+
+  createRandomPoints(n){
+    for (let i = 0; i<2500; i++){
+      this.changeGrid(i, 0);
+    }
+    for (let i = 0; i<n; i++){
+      let x = Math.floor(Math.random()*50);
+      let y = Math.floor(Math.random()*50);
+      this.changeGrid(50*y+x, 1);
+    }
+  }
 
   constructor(props) {
     super(props);
     this.state = {
-      algType: 0
+      algType: 0,
+      n: 1,
     };
   }
 
@@ -37,20 +207,40 @@ export default class ClusteringDemo extends React.Component {
       <Box style={{
           position: "absolute"
         }}>
+        <Stack direction="row" spacing={3}>
+        <Box>
         <Slide in={this.state.algType === 0} timeout={500} direction="down" mountOnEnter unmountOnExit>
           <InputLabel id="pagination-label" shrink={true}>Number of Clusters</InputLabel>
         </Slide>
-        <Slide in={this.state.algType === 0} timeout={500} direction="down" mountOnEnter unmountOnExit>
+        <Slide in={this.state.algType === 0} onChange={(e,p)=>{this.setState({n: p})}} timeout={500} direction="down" mountOnEnter unmountOnExit>
           <Pagination count={10} color="primary"/>
         </Slide>
+        </Box>
+        <Slide in={this.state.algType === 0} timeout={500} direction="down" mountOnEnter unmountOnExit>
+          <ButtonGroup variant="contained" aria-label="outlined primary button group">
+            <Button onClick={()=>{this.k_means(this.state.n)}}>Run Algorithm</Button>
+            <Button onClick={()=>{this.clearGrid(0)}}>Clear Points</Button>
+          </ButtonGroup>
+        </Slide>
+        </Stack>
       </Box>
+
       <Box style={{
           position: "absolute"
         }}>
+        <Stack direction="row" spacing={3}>
         <Slide in={this.state.algType === 2} timeout={500} direction="down" mountOnEnter unmountOnExit>
           <TextField id="mean-shift-radius" label="Radius" variant="standard"/>
         </Slide>
+        <Slide in={this.state.algType === 2} timeout={500} direction="down" mountOnEnter unmountOnExit>
+          <ButtonGroup variant="contained" aria-label="outlined primary button group">
+            <Button>Run Algorithm</Button>
+            <Button onClick={()=>{this.clearGrid(0)}}>Clear Points</Button>
+          </ButtonGroup>
+        </Slide>
+        </Stack>
       </Box>
+
       <Box style={{
           position: "absolute"
         }}>
@@ -60,6 +250,12 @@ export default class ClusteringDemo extends React.Component {
           </Slide>
           <Slide id="alg3" in={this.state.algType === 3} timeout={500} direction="down" mountOnEnter unmountOnExit>
             <TextField id="DBSCAN-ppc" label="Points per cluster" variant="standard"/>
+          </Slide>
+          <Slide in={this.state.algType === 3} timeout={500} direction="down" mountOnEnter unmountOnExit>
+            <ButtonGroup variant="contained" aria-label="outlined primary button group">
+              <Button>Run Algorithm</Button>
+              <Button onClick={()=>{this.clearGrid(0)}}>Clear Points</Button>
+            </ButtonGroup>
           </Slide>
         </Stack>
       </Box>
@@ -93,7 +289,8 @@ export default class ClusteringDemo extends React.Component {
       <Paper elevation={6} sx={{display: "inline-block", width: "23vw", height: "45vw", backgroundColor: "#e3fdff", margin: "1vw"}}>
       </Paper>
       <Paper elevation={6} sx={{display: "inline-block", width: "45vw", height: "45vw", backgroundColor: "#e3fdff", margin: "1vw"}}>
-        <Grid style={{width: "45vw", height: "45vw", borderRadius: "1px"}} background="#e3fdff">
+        <Grid style={{width: "45vw", height: "45vw", borderRadius: "1px"}} background="#e3fdff" gridState={this.gridState}
+                    changeGrid={this.changeGrid} gridAnimations={this.gridAnimations} deleteAnimation={this.deleteAnimation}>
         </Grid>
       </Paper>
       <Paper elevation={6} sx={{display: "inline-block", width: "23vw", height: "45vw", backgroundColor: "#e3fdff", margin: "1vw"}}>
--- src/Grid.js
+++ src/Grid.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import {PtsCanvas} from 'react-pts-canvas';
-import {Pt, Group, Line, Create, Sound, Triangle, Const, Geom} from 'pts/dist/es5';
+import {Pt, Group, Line, Create, Sound, Triangle, Const, Geom, Color, Rectangle} from 'pts/dist/es5';
 
 export default class Grid extends PtsCanvas{
-
+  mousedown = false;
+  mouseup = false;
+  colorMappings = ['white', "black", "blue", "red", "green", "purple", "yellow"];
   constructor() {
     super();
-    this.colorGrid = new Array(10000).fill(0);
-    this.colorMappings = ['#e0fffe']
-  }
-
-  _create() {
-    // Create a line and a grid, and convert them to `Noise` points
-    this.gd = Create.gridPts( this.space.innerBound, 50, 50 );
   }
 
   colorOfKey(key){
     return this.colorMappings[key]
   }
+
+  canvasClicked = (function(event){
+    let elem = this.space.element,
+    elemLeft = elem.offsetLeft + elem.clientLeft,
+    elemTop = elem.offsetTop + elem.clientTop,
+    context = elem.getContext('2d');
+    let x = event.pageX - elemLeft,
+        y = event.pageY - elemTop;
+    let bar = Math.floor(x*50/this.space.width);
+    let foo = Math.floor(y*50/this.space.width);
+    let currentIndex = 50 * foo + bar;
+    let current = this.props.gridState[currentIndex];
+    this.props.changeGrid(currentIndex,(current + 1)%2);
+    this.mousedown = true;
+  }).bind(this);
 
   componentDidUpdate() {
     if (this.props.pause) {
@@ -27,30 +37,46 @@ export default class Grid extends PtsCanvas{
     }
   }
 
-
   // Override PtsCanvas' start function
   start(space, bound) {
-    this._create();
+    this.gd = Create.gridPts( this.space.innerBound, 50, 50 );
+    this.width = this.space.width;
+    this.space.element.addEventListener("mousedown", this.canvasClicked, false);
   }
-
 
   // Override PtsCanvas' resize function
   resize() {
-    this._create();
+    this.gd = Create.gridPts( this.space.innerBound, 50, 50 );
+    this.width = this.space.width;
   }
 
+  coordChange(point){
+    return [this.width*point[0]/50, this.width*point[1]/50];
+  }
   // Override PtsCanvas' animate function
   animate(time, ftime) {
     // Use pointer position to change speed
-    let speed = this.space.pointer.$subtract( this.space.center ).divide( this.space.center ).abs();
-    // Generate noise in a grid
+    let size = this.width/105;
     for (const[key, value] of this.gd.entries()){
-      this.form.fillOnly(this.colorOfKey(this.colorGrid[key])).point(value, 5, "square" );
+      this.form.fillOnly(this.colorOfKey(this.props.gridState[key])).point(value, size, "square" );
+    }
+    for (const [index, animation] of this.props.gridAnimations.entries()){
+      console.log("in animate");
+      console.log(animation.type);
+      if (animation.progress >= animation.duration){this.props.deleteAnimation(index)}
+      if (animation.type == "dot"){
+        let progress = Math.min(animation.progress/animation.duration,1)
+        this.form.fillOnly(animation.color).point(this.gd[animation.index], size-size*progress, "circle" );
+      } else if (animation.type == "x"){
+        let coordinates = this.coordChange(animation.position);
+        let rect = Rectangle.corners( Rectangle.fromCenter( coordinates, size*3 ) ).rotate2D( 1.3, this.coordChange(animation.position) );
+        this.form.strokeOnly(this.colorMappings[animation.color], size*3/2).lines( [ [rect[0], rect[2]], [rect[1], rect[3]] ] );
+        this.form.strokeOnly("white", size/4).lines( [ [rect[0], rect[2]], [rect[1], rect[3]] ] );
+      }
+      animation.progress += ftime;
     }
   }
 
   action(type, x, y){
-
   }
-
 }
